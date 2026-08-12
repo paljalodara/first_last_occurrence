3cede64cf1c0d7d47f5fdea35c1493fb0f5563b4
Pal Jalodara <202401074@dau.ac.in>
2026-08-12 12:59:26 +0530

    Fix first and last occurrence binary search

--- first_last_occurrence.cpp
+++ first_last_occurrence.cpp
@@ -4,8 +4,8 @@ using namespace std;
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = findLast(nums, target); 
-        int last = findFirst(nums, target); 
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
 
         return {first, last};
     }
@@ -15,7 +15,7 @@ private:
         int low = 0, high = nums.size() - 1;
         int ans = -1;
 
-        while (low < high) { 
+        while (low <= high) {
             int mid = low + (high - low) / 2;
 
             if (nums[mid] == target) {
@@ -42,7 +42,7 @@ private:
 
             if (nums[mid] == target) {
                 ans = mid;
-                high = mid - 1; 
+                low = mid + 1;
             }
             else if (nums[mid] < target) {
                 low = mid + 1;
